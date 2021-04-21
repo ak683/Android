ce71036f38cd1db1cf8bb576efb74ef9918eed65
ak683 <82591926+ak683@users.noreply.github.com>
2021-04-21 17:41:50 +0300

    Create Main.java

---
+++ task_2.6/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        if (a%b==0) System.out.println("Делится"+' '+ a/b);
+        else   System.out.println("Делится с остатком"+' '+ a%b);
