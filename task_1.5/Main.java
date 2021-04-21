e28bf7620f40cc82f49346adb57855faa98b6baf
ak683 <82591926+ak683@users.noreply.github.com>
2021-04-21 17:20:36 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int TC = scan.nextInt();
+        System.out.println(1.8*TC + 32);
+    }
+}
