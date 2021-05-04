101eeb8b86f7f77700d08382e43d59a1359393fe
ak683 <82591926+ak683@users.noreply.github.com>
2021-05-04 16:56:33 +0300

    Create Main.java

---
+++ task_4.3/Main.java
@@ -0,0 +1,52 @@
+/*
+Реализовать метод boolean fight(Cat anotherCat):
+реализовать механизм драки котов в зависимости от их веса, возраста и силы.
+Нужно сравнить каждый критерий по отдельности, и победителем будет тот, у которого больше "победивших" критериев.
+Метод должен определять, выиграли ли мы (this) бой или нет, т.е. возвращать true, если выиграли и false - если нет.
+Если ничья и никто не выиграл, возвращаем либо true либо false, по вашему усмотрению
+Требования:
+•	Класс Cat должен содержать конструктор без параметров.
+•	Класс Cat должен содержать всего три поля: age, weight и strength. Поля должны быть публичные.
+•	В классе Cat должен быть метод fight.
+•	В методе fight реализовать механизм драки котов в зависимости от их веса, возраста и силы согласно условию.
+•	Метод должен возвращать одно и тоже значение, если мы деремся с одним и тем же котом.
+•	Если некий кот1 выигрывает у кота кот2, то кот2 должен проигрывать коту кот1.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat(2, 4, 5);
+        Cat cat2 = new Cat(3, 3, 3);
+
+        System.out.println(cat1.fight(cat2));
+
+    }
+}
+
+class Cat {
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Cat(int age, int weight, int strength) {
+        this.age = age;
+        this.weight = weight;
+        this.strength = strength;
+    }
+
+    public boolean fight(Cat anotherCat) {
+        //напишите тут ваш код
+        int sum1=0;
+        int sum2=0;
+        if (this.age > anotherCat.age) sum1++;
+        else sum2++;
+        if (this.weight > anotherCat.weight) sum1++;
+        else sum2++;
+        if (this.strength > anotherCat.strength) sum1++;
+        else sum2++;
+        if (sum1>sum2) return true;
+        else return false;
+
+
+    }
+}
